0f83827e38bd1c9af1b9c72d2e12ef77dee00bbb
Bao Quan <qmnguyen@iu.edu>
2023-03-14 20:53:54 +0000

    Add test cases for exercise 01.

--- hw01/test/HW01Tests.hs
+++ hw01/test/HW01Tests.hs
@@ -1,15 +1,42 @@
 module Main where
-import ValidatingCreditCards
-import Test.HUnit
-import qualified System.Exit as Exit
 
+import System.Exit
+import Test.HUnit
+import ValidatingCreditCards
+
+-- Exercise 1
+-- Tests for `toDigits()`.
 testNegativeNumber :: Test
 testNegativeNumber = TestCase (assertEqual "should return empty list" [] (toDigits (-17)))
 
-tests :: Test
-tests = TestList [TestLabel "testNegativeNumber" testNegativeNumber]
+testZeroNumber :: Test
+testZeroNumber = TestCase (assertEqual "should return empty list" [] (toDigits 0))
 
-main :: IO()
+testNumberToDigits :: Test
+testNumberToDigits = TestCase (assertEqual "should return correct order" [1, 2, 3, 4] (toDigits 1234))
+
+-- Tests for `toDigitsRev()`.
+testNegativeNumberRev :: Test
+testNegativeNumberRev = TestCase (assertEqual "should return empty list" [] (toDigitsRev (-17)))
+
+testZeroNumberRev :: Test
+testZeroNumberRev = TestCase (assertEqual "should return empty list" [] (toDigitsRev 0))
+
+testNumberToDigitsRev :: Test
+testNumberToDigitsRev = TestCase (assertEqual "should return correct order" [4, 3, 2, 1] (toDigitsRev 1234))
+
+tests :: Test
+tests =
+  TestList
+    [ TestLabel "testNegativeNumber" testNegativeNumber,
+      TestLabel "testZeroNumber" testZeroNumber,
+      TestLabel "testNumberToDigits" testNumberToDigits,
+      TestLabel "testNegativeNumberRev" testNegativeNumberRev,
+      TestLabel "testZeroNumberRev" testZeroNumberRev,
+      TestLabel "testNumberToDigitsRev" testNumberToDigitsRev
+    ]
+
+main :: IO ()
 main = do
-    result <- runTestTT tests
-    if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
+  result <- runTestTT tests
+  if failures result > 0 then exitFailure else exitSuccess
