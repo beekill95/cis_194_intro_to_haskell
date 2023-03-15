a9e35ff9f059e8df438c1f767a0dd09759494c60
Bao Quan <qmnguyen@iu.edu>
2023-03-15 17:22:42 +0000

    Finish exercise 6.

--- hw01/src/HanoiTowers.hs
+++ hw01/src/HanoiTowers.hs
@@ -4,6 +4,7 @@ type Peg = String
 
 type Move = (Peg, Peg)
 
+-- Exercise 5: Hanoi Towers with 3 pegs.
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n from temporary to
   | n == 0 = []
@@ -12,3 +13,14 @@ hanoi n from temporary to
       hanoi (n - 1) from to temporary
         ++ [(from, to)]
         ++ hanoi (n - 1) temporary from to
+
+-- Exercise 6: Hanoi Towers with 4 pegs.
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 n from temp1 temp2 dest
+  | n == 0 = []
+  | n == 1 = [(from, dest)]
+  | n == 2 = [(from, temp1), (from, dest), (temp1, dest)]
+  | otherwise =
+      hanoi4 (n - 2) from temp2 dest temp1
+        ++ [(from, temp2), (from, dest), (temp2, dest)]
+        ++ hanoi4 (n - 2) temp1 from temp2 dest
--- hw01/test/HanoiTowersTests.hs
+++ hw01/test/HanoiTowersTests.hs
@@ -3,6 +3,7 @@ module HanoiTowersTests (hanoiTowersTests) where
 import HanoiTowers
 import Test.HUnit
 
+-- Exercise 5: Hanoi Towers with 3 pegs.
 testEmptyDisks :: Test
 testEmptyDisks =
   TestCase
@@ -36,11 +37,58 @@ testThreeDisks =
         (hanoi 3 "a" "b" "c")
     )
 
+-- Exercise 6: Hanoi Towers with 4 pegs.
+testHanoi4EmptyDisk :: Test
+testHanoi4EmptyDisk =
+  TestCase
+    ( assertEqual "should return no moves" [] (hanoi4 0 "a" "b" "c" "d")
+    )
+
+testHanoi4OneDisk :: Test
+testHanoi4OneDisk =
+  TestCase
+    ( assertEqual "should return one move" [("a", "d")] (hanoi4 1 "a" "b" "c" "d")
+    )
+
+testHanoi4TwoDisks :: Test
+testHanoi4TwoDisks =
+  TestCase
+    ( assertEqual
+        "should return three moves"
+        [("a", "b"), ("a", "d"), ("b", "d")]
+        (hanoi4 2 "a" "b" "c" "d")
+    )
+
+testHanoi4ThreeDisks :: Test
+testHanoi4ThreeDisks =
+  TestCase
+    ( assertEqual
+        "should return 5 moves"
+        [("a", "b"), ("a", "c"), ("a", "d"), ("c", "d"), ("b", "d")]
+        (hanoi4 3 "a" "b" "c" "d")
+    )
+
+testHanoi4FourDisks :: Test
+testHanoi4FourDisks =
+  TestCase
+    ( assertEqual
+        "should return 9 moves"
+        [("a", "c"), ("a", "b"), ("c", "b"), ("a", "c"), ("a", "d"), ("c", "d"), ("b", "a"), ("b", "d"), ("a", "d")]
+        (hanoi4 4 "a" "b" "c" "d")
+    )
+
+-- Collect all Hanoi Towers tests.
 hanoiTowersTests :: Test
 hanoiTowersTests =
   TestList
     [ TestLabel "testEmptyDisks" testEmptyDisks,
       TestLabel "testOneDisk" testOneDisk,
       TestLabel "testTwoDisks" testTwoDisks,
-      TestLabel "testThreeDisks" testThreeDisks
+      TestLabel "testThreeDisks" testThreeDisks,
+      -- Hanoi Towers 4 pegs.
+      TestLabel "testHanoi4NoDisk" testHanoi4EmptyDisk,
+      TestLabel "testHanoi4OneDisk" testHanoi4OneDisk,
+      TestLabel "testHanoi4TwoDisks" testHanoi4TwoDisks,
+      TestLabel "testHanoi4ThreeDisks" testHanoi4ThreeDisks,
+      TestLabel "testHanoi4FourDisks" testHanoi4FourDisks
     ]
