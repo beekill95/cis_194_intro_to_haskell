646cab424e15437d6fe186224fd1a2f3beec1020
Bao Quan <qmnguyen@iu.edu>
2023-03-16 14:30:46 +0000

    Finish hw02.

--- hw02/app/Main.hs
+++ hw02/app/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Log
+import LogAnalysis
+
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
+main = testWhatWentWrong parse whatWentWrong "data/error.log" >>= print
