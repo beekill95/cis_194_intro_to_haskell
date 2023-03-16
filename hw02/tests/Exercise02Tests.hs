6d256fba5c109ed8f254f7ebc6b25058755bbc1c
beekill <nguyenmbquan95@gmail.com>
2023-03-16 07:03:46 -0400

    Finish exercise 02.

--- hw02/tests/Exercise02Tests.hs
+++ hw02/tests/Exercise02Tests.hs
@@ -51,5 +51,41 @@ exercise02Tests =
                 (Node Leaf (Unknown "message") Leaf)
                 (insert (LogMessage Info (-1) "message") (Node Leaf (Unknown "message") Leaf))
             )
+        ),
+      TestLabel
+        "test insert a valid message to a leaf node"
+        ( TestCase
+            ( assertEqual
+                "should return a new tree with the message"
+                (Node Leaf (LogMessage Info 5 "message") Leaf)
+                (insert (LogMessage Info 5 "message") Leaf)
+            )
+        ),
+      TestLabel
+        "test insert a valid message to the left node"
+        ( TestCase
+            ( assertEqual
+                "should return a new tree with the message inserted to the left"
+                (Node (Node Leaf (LogMessage Info 4 "new message") Leaf) (LogMessage Info 5 "message") Leaf)
+                (insert (LogMessage Info 4 "new message") (Node Leaf (LogMessage Info 5 "message") Leaf))
+            )
+        ),
+      TestLabel
+        "test insert a message with the same timestamp"
+        ( TestCase
+            ( assertEqual
+                "should return a new tree with the message inserted to the left"
+                (Node (Node Leaf (LogMessage Info 5 "new message") Leaf) (LogMessage Info 5 "message") Leaf)
+                (insert (LogMessage Info 5 "new message") (Node Leaf (LogMessage Info 5 "message") Leaf))
+            )
+        ),
+      TestLabel
+        "test insert a valid message the the right node"
+        ( TestCase
+            ( assertEqual
+                "should return a new tree with the message inserted to the right"
+                (Node Leaf (LogMessage Info 5 "message") (Node Leaf (LogMessage Info 6 "new message") Leaf))
+                (insert (LogMessage Info 6 "new message") (Node Leaf (LogMessage Info 5 "message") Leaf))
+            )
         )
     ]
