73c1260c60b0f83d5941db05d16f3c2bf2eb4872
Bao Quan <qmnguyen@iu.edu>
2023-03-17 13:41:11 +0000

    Finish exercise 1.

--- hw03/src/Golf.hs
+++ hw03/src/Golf.hs
@@ -2,7 +2,15 @@ module Golf (skips) where
 
 -- Exercise 1 Hopscotch
 skips :: [a] -> [[a]]
-skips [] = []
-skips (head : rest) = (head : rest) : skips rest
+skips elements = map (uncurry takeEvery) elementsWithHopLength
   where
-    enumerate = zip [1 ..]
+    elementsWithHopLength = zip [0 ..] $ replicate nbElements elements
+    nbElements = length elements
+
+takeEvery :: Int -> [a] -> [a]
+takeEvery _ [] = []
+takeEvery n elements = case remainedElements of
+  [] -> []
+  (first : rest) -> first : takeEvery n rest
+  where
+    remainedElements = drop n elements
