4d596f5f588d93f6b56a779c09a539b554e17146
Bao Quan <qmnguyen@iu.edu>
2023-03-22 16:57:26 +0000

    Exercise 3 and 4 wip.

--- hw05/src/Calc.hs
+++ hw05/src/Calc.hs
@@ -14,3 +14,40 @@ evalStr :: String -> Maybe Integer
 evalStr s = eval <$> strToExprT s
   where
     strToExprT = parseExp Lit Add Mul
+
+-- Exercise 3: Type class expression Expr.
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
+-- Exercise 4: Make Bool, MinMax, and Mod7 as instances of Expr.
+instance Expr Bool where
+  lit n
+    | n <= 0 = True
+    | otherwise = False
+
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax lhs) (MinMax rhs) = lit $ max lhs rhs
+  mul (MinMax lhs) (MinMax rhs) = lit $ min lhs rhs
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit = Mod7
+  add (Mod7 lhs) (Mod7 rhs) = lit $ (lhs + rhs) `mod` 7
+  mul (Mod7 lhs) (Mod7 rhs) = lit $ (lhs * rhs) `mod` 7
