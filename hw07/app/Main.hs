0123afc464094d56685dde1d5846a28c1586bddc
beekill <nguyenmbquan95@gmail.com>
2023-03-27 19:22:45 -0400

    Switch to use JoinList backed buffer.

--- hw07/app/Main.hs
+++ hw07/app/Main.hs
@@ -1,13 +1,23 @@
 module Main where
 
+-- import StringBuffer
+
+import Buffer (Buffer (fromString))
 import Editor
-import StringBuffer
+import JoinList
+import Scrabble
+import Sized
+
+reify :: JoinList (Score, Size) String -> JoinList (Score, Size) String
+reify = id
 
 main =
   runEditor editor $
-    unlines
-      [ "This buffer is for notes you don't want to save, and for",
-        "evaluation of steam valve coefficients.",
-        "To load a different file, type the character L followed",
-        "by the name of the file."
-      ]
+    reify $
+      fromString $
+        unlines
+          [ "This buffer is for notes you don't want to save, and for",
+            "evaluation of steam valve coefficients.",
+            "To load a different file, type the character L followed",
+            "by the name of the file."
+          ]
