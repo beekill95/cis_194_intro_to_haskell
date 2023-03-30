c8c520d2f786f5f0a5120864a97789725ca6e714
Bao Quan <qmnguyen@iu.edu>
2023-03-30 19:33:15 +0000

    Finish hw10.

--- hw10/tests/Exercise05.hs
+++ hw10/tests/Exercise05.hs
@@ -4,4 +4,24 @@ import AParser
 import Test.HUnit
 
 exercise05Tests :: Test
-exercise05Tests = TestList []
+exercise05Tests =
+  TestList
+    [ TestLabel "> Test parse integer <" $
+        TestCase $
+          assertEqual
+            "should parse integer and return empty"
+            (Just ((), "abcd"))
+            (runParser intOrUppercase "342abcd"),
+      TestLabel "> Test parse uppercase <" $
+        TestCase $
+          assertEqual
+            "should parse an uppercase character and return empty"
+            (Just ((), "YZ"))
+            (runParser intOrUppercase "XYZ"),
+      TestLabel "> Test parse invalid string <" $
+        TestCase $
+          assertEqual
+            "should return Nothing"
+            Nothing
+            (runParser intOrUppercase "foo")
+    ]
