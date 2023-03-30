11a2fa10e4b9d0a3f02c1e13f9abf570ca469f5d
Bao Quan <qmnguyen@iu.edu>
2023-03-30 19:29:02 +0000

    Unit tests for exercise 4.

--- hw10/tests/Exercise04.hs
+++ hw10/tests/Exercise04.hs
@@ -1,7 +1,22 @@
 module Exercise04 where
 
 import AParser
+import Control.Applicative
 import Test.HUnit
 
 exercise04Tests :: Test
-exercise04Tests = TestList []
+exercise04Tests =
+  TestList
+    [ TestLabel "> Test `empty` 1 <" $
+        TestCase $
+          assertEqual
+            "should return a parser always fails"
+            Nothing
+            (runParser (empty :: Parser Char) "abc"),
+      TestLabel "> Test `empty` 2 <" $
+        TestCase $
+          assertEqual
+            "should return a parser always fails"
+            Nothing
+            (runParser (empty :: Parser Int) "123")
+    ]
