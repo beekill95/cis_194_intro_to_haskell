a6c76e5255c291697a23189061c71d0925279c4c
Bao Quan <qmnguyen@iu.edu>
2023-03-31 17:04:58 +0000

    Finish hw11.

--- hw11/app/Main.hs
+++ hw11/app/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+import AParser (Parser (runParser))
+import Control.Monad (forever)
+import SExpr
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  putStrLn "Simple S-expr Parser"
+  putStrLn "Input S-expr in the prompt. Quit using Ctrl-C."
+  forever $ do
+    putStr "> "
+    l <- getLine
+    print $ runParser parseSExpr l
