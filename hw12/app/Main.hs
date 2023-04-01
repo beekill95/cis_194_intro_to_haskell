c0c9f03a79ca8c0353c6b7d5f1528c306b0cea9e
beekill <nguyenmbquan95@gmail.com>
2023-04-01 19:16:03 -0400

    Implement cli.

--- hw12/app/Main.hs
+++ hw12/app/Main.hs
@@ -1,5 +1,26 @@
 module Main where
 
+import Control.Monad.Random
+import Risk
+import System.IO
+import Text.Read (readMaybe)
+
+prompt :: String -> IO String
+prompt text = do
+  putStr text
+  hFlush stdout
+  getLine
+
+armySize :: String -> IO Army
+armySize p = do
+  size <- readMaybe <$> prompt p
+  case size of
+    Just s -> return s
+    Nothing -> armySize p
+
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  attackers <- armySize "Attackers: "
+  defenders <- armySize "Defenders: "
+  prob <- fmap show $ evalRandIO $ successProb $ Battlefield attackers defenders
+  putStrLn $ "Success prob: " ++ prob
