8626937b5f1bf2b42a87aef8568b1d279fc96033
Bao Quan <qmnguyen@iu.edu>
2023-04-13 15:43:00 +0000

    Finish ex01.

--- fa14_hw09_testing/tests/RingTests.hs
+++ fa14_hw09_testing/tests/RingTests.hs
@@ -2,3 +2,13 @@ module RingTests where
 
 import Ring
 import Test.QuickCheck
+
+-- Exercise 1: Write Arbitrary instances for Mod5 and Mat2x2.
+instance Arbitrary Mod5 where
+  arbitrary = mkMod <$> arbitrary
+
+instance Arbitrary Mat2x2 where
+  arbitrary = MkMat <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+  -- Exercise 2: Write `shrink` method for Mat2x2.
+  shrink _ = []
