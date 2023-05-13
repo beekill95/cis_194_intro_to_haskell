bd167a54662c4d055c5b40e77d95d94f9bcad869
beekill <nguyenmbquan95@gmail.com>
2023-05-13 06:31:13 -0700

    Finish exercise 6.

--- fa14_hw09_testing/src/BST.hs
+++ fa14_hw09_testing/src/BST.hs
@@ -16,31 +16,48 @@ isEmpty :: BST a -> Bool
 isEmpty Leaf = True
 isEmpty _ = False
 
+-- | Exercise 6: Rewritten `isBSTBetween` and `isBST` using Ord type class.
 -- | Is the tree a BST between the given endpoints?
-isBSTBetween ::
-  -- | comparison function
-  (a -> a -> Ordering) ->
-  -- | lower bound, if one exists
-  Maybe a ->
-  -- | upper bound, if one exists
-  Maybe a ->
-  -- | tree to test
-  BST a ->
-  Bool
-isBSTBetween _ _ _ Leaf = True
-isBSTBetween cmp m_lower m_upper (Node left x right) =
-  isBSTBetween cmp m_lower (Just x) left
-    && isBSTBetween cmp (Just x) m_upper right
+-- isBSTBetween' ::
+--   -- | comparison function
+--   (a -> a -> Ordering) ->
+--   -- | lower bound, if one exists
+--   Maybe a ->
+--   -- | upper bound, if one exists
+--   Maybe a ->
+--   -- | tree to test
+--   BST a ->
+--   Bool
+-- isBSTBetween' _ _ _ Leaf = True
+-- isBSTBetween' cmp m_lower m_upper (Node left x right) =
+--   isBSTBetween' cmp m_lower (Just x) left
+--     && isBSTBetween' cmp (Just x) m_upper right
+--     && case m_lower of
+--       Just lower -> lower `cmp` x /= GT
+--       Nothing -> True
+--     && case m_upper of
+--       Just upper -> x `cmp` upper /= GT
+--       Nothing -> True
+
+-- | Is this a valid BST?
+-- isBST :: (a -> a -> Ordering) -> BST a -> Bool
+-- isBST cmp = isBSTBetween cmp Nothing Nothing
+
+-- | Is the tree a BST between the given endpoints?
+isBSTBetween :: Ord a => Maybe a -> Maybe a -> BST a -> Bool
+isBSTBetween m_lower m_upper (Node left x right) =
+  isBSTBetween m_lower (Just x) left
+    && isBSTBetween (Just x) m_upper right
     && case m_lower of
-      Just lower -> lower `cmp` x /= GT
+      Just lower -> lower <= x
       Nothing -> True
     && case m_upper of
-      Just upper -> x `cmp` upper /= GT
+      Just upper -> x <= upper
       Nothing -> True
 
 -- | Is this a valid BST?
-isBST :: (a -> a -> Ordering) -> BST a -> Bool
-isBST cmp = isBSTBetween cmp Nothing Nothing
+isBST :: Ord a => BST a -> Bool
+isBST = isBSTBetween Nothing Nothing
 
 -- | Get a list of the elements in sorted order
 getElements :: BST a -> [a]
