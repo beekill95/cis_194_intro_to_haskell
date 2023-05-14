87448bf3d941990cd6f31127b0447cb028d332e4
beekill <nguyenmbquan95@gmail.com>
2023-05-14 10:50:43 -0700

    Finish hw09

--- fa14_hw09_testing/src/Ring.hs
+++ fa14_hw09_testing/src/Ring.hs
@@ -3,21 +3,21 @@
 
 module Ring where
 
- -- You can optionally include a list of symbols after an import statement
- -- to say exactly what you're importing from the other module. This is sometimes
- -- useful for documentation, and to avoid name clashes between modules.
-import Control.Arrow ( first )
-import Data.Maybe    ( listToMaybe )
-import Data.List     ( stripPrefix )
+-- You can optionally include a list of symbols after an import statement
+-- to say exactly what you're importing from the other module. This is sometimes
+-- useful for documentation, and to avoid name clashes between modules.
+import Control.Arrow (first)
+import Data.List (stripPrefix)
+import Data.Maybe (listToMaybe)
 
 -- Definition for rings
 class Ring a where
-  addId  :: a            -- additive identity
-  addInv :: a -> a       -- additive inverse
-  mulId  :: a            -- multiplicative identity
+  addId :: a -- additive identity
+  addInv :: a -> a -- additive inverse
+  mulId :: a -- multiplicative identity
 
-  add :: a -> a -> a     -- addition
-  mul :: a -> a -> a     -- multiplication
+  add :: a -> a -> a -- addition
+  mul :: a -> a -> a -- multiplication
 
 -- Something is parsable if there is a way to read it in from a string
 class Parsable a where
@@ -28,21 +28,23 @@ class Parsable a where
 
 -- | Use the 'parse' method to parse all of a string, requiring that there
 -- are no leftovers.
-parseAll :: Parsable a => String -> Maybe a
-parseAll str = do   -- using the Maybe monad
+parseAll :: (Parsable a) => String -> Maybe a
+parseAll str = do
+  -- using the Maybe monad
   (result, "") <- parse str
   return result
 
 instance Parsable Integer where
   parse = listToMaybe . reads
-    -- Look up these functions online. Think about why this combination works.
-    -- (It should work for any member of the `Read` class. Like `Integer`.)
+
+-- Look up these functions online. Think about why this combination works.
+-- (It should work for any member of the `Read` class. Like `Integer`.)
 
 -- The canonical instance for integers:
 instance Ring Integer where
-  addId  = 0
+  addId = 0
   addInv = negate
-  mulId  = 1
+  mulId = 1
 
   add = (+)
   mul = (*)
@@ -58,53 +60,55 @@ mkMod :: Integer -> Mod5
 mkMod = MkMod . (`mod` 5)
 
 instance Ring Mod5 where
-  addId  = mkMod 0
+  addId = mkMod 0
   addInv = mkMod . negate . unMod
-  mulId  = mkMod 1
+  mulId = mkMod 1
 
   add x y = mkMod (unMod x + unMod y)
   mul x y = mkMod (unMod x * unMod y)
 
 instance Parsable Mod5 where
-  parse str = do (n, rest) <- parse str
-                 return (mkMod n, rest)
+  parse str = do
+    (n, rest) <- parse str
+    return (mkMod n, rest)
 
 -- 2x2 matrices
 data Mat2x2 = MkMat Integer Integer Integer Integer
   deriving (Show, Eq)
 
 instance Ring Mat2x2 where
-  addId  = MkMat 0 0 0 0
+  addId = MkMat 0 0 0 0
   addInv (MkMat a b c d) = MkMat (negate a) (negate b) (negate c) (negate d)
-  mulId  = MkMat 1 0 0 1
+  mulId = MkMat 1 0 0 1
 
-  add (MkMat a1 b1 c1 d1) (MkMat a2 b2 c2 d2)
-    = MkMat (a1 + a2) (b1 + b2) (c1 + c2) (d1 + d2)
+  add (MkMat a1 b1 c1 d1) (MkMat a2 b2 c2 d2) =
+    MkMat (a1 + a2) (b1 + b2) (c1 + c2) (d1 + d2)
 
-  mul (MkMat a1 b1 c1 d1) (MkMat a2 b2 c2 d2)
-    = MkMat (a1 * a2 + b1 * c2)
-            (a1 * b2 + b1 * d2)
-            (c1 * a2 + d1 * c2)
-            (c1 * b2 + d1 * d2)
+  mul (MkMat a1 b1 c1 d1) (MkMat a2 b2 c2 d2) =
+    MkMat
+      (a1 * a2 + b1 * c2)
+      (a1 * b2 + b1 * d2)
+      (c1 * a2 + d1 * c2)
+      (c1 * b2 + d1 * d2)
 
 instance Parsable Mat2x2 where
   parse str = do
-    str      <- stripPrefix "[[" str
+    str <- stripPrefix "[[" str
     (a, str) <- parse str
-    str      <- stripPrefix ","  str
+    str <- stripPrefix "," str
     (b, str) <- parse str
-    str      <- stripPrefix "][" str
+    str <- stripPrefix "][" str
     (c, str) <- parse str
-    str      <- stripPrefix ","  str
+    str <- stripPrefix "," str
     (d, str) <- parse str
-    str      <- stripPrefix "]]" str
+    str <- stripPrefix "]]" str
     return (MkMat a b c d, str)
 
 -- Boolean algebra
 instance Ring Bool where
-  addId  = False
+  addId = False
   addInv = not
-  mulId  = True
+  mulId = True
 
   add = (||)
   mul = (&&)
--- fa14_hw09_testing/tests/ParserTests.hs
+++ fa14_hw09_testing/tests/ParserTests.hs
@@ -9,16 +9,76 @@ import Test.HUnit
 integerParserTests :: Test
 integerParserTests =
   TestList
-    []
+    [ TestLabel "Read valid integer" $
+        TestCase $
+          assertEqual
+            "should return correct integer"
+            (Just (177 :: Integer, "abc"))
+            (parse "177abc"),
+      TestLabel "Read invalid integer" $
+        TestCase $
+          assertEqual
+            "should return Nothing"
+            (Nothing :: Maybe (Integer, String))
+            (parse "abc177")
+    ]
 
 mod5ParserTests :: Test
-mod5ParserTests = TestList []
+mod5ParserTests =
+  TestList
+    [ TestLabel "Read valid Mod5" $
+        TestCase $
+          assertEqual
+            "should return correct value"
+            (Just (mkMod 177, "abc"))
+            (parse "177abc"),
+      TestLabel "Read invalid Mod5" $
+        TestCase $
+          assertEqual
+            "should return Nothing"
+            (Nothing :: Maybe (Mod5, String))
+            (parse "abc177")
+    ]
 
 mat2x2ParserTests :: Test
-mat2x2ParserTests = TestList []
+mat2x2ParserTests =
+  TestList
+    [ TestLabel "Read valid Mat2x2" $
+        TestCase $
+          assertEqual
+            "should return correct Mat2x2"
+            (Just (MkMat 2 3 1 4, "abc"))
+            (parse "[[2,3][1,4]]abc"),
+      TestLabel "Read invalid Mat2x2" $
+        TestCase $
+          assertEqual
+            "should return correct Mat2x2"
+            (Nothing :: Maybe (Mat2x2, String))
+            (parse "[[2,3]1,4]]abc")
+    ]
 
 boolParserTests :: Test
-boolParserTests = TestList []
+boolParserTests =
+  TestList
+    [ TestLabel "Read valid boolean" $
+        TestCase $
+          assertEqual
+            "should parse True"
+            (Just (True, " abc"))
+            (parse "True abc"),
+      TestLabel "Read valid boolean" $
+        TestCase $
+          assertEqual
+            "should return False"
+            (Just (False, " abc"))
+            (parse "False abc"),
+      TestLabel "Read invalid boolean" $
+        TestCase $
+          assertEqual
+            "should return Nothing"
+            (Nothing :: Maybe (Bool, String))
+            (parse "177abc")
+    ]
 
 -- Collect all tests.
 parserTests =
