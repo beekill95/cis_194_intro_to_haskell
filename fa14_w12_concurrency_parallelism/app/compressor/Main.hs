c7060039a514c7bb37cab264f42e7a663012657e
beekill <nguyenmbquan95@gmail.com>
2023-05-14 16:09:48 -0700

    WIP: compressor

--- fa14_w12_concurrency_parallelism/app/compressor/Main.hs
+++ fa14_w12_concurrency_parallelism/app/compressor/Main.hs
@@ -1,5 +1,23 @@
 module Main where
 
 import Codec.Compression.GZip (compress)
+import Control.Concurrent (forkIO)
+import Control.Exception
+import Control.Monad (forever)
+import qualified Data.ByteString.Lazy as L
 
-main = putStrLn "Hello, World"
+main :: IO ()
+main = do
+  putStrLn "Enter a file to compress> "
+  line <- getLine
+  case line of
+    "" -> return ()
+    name -> do
+      handle print $ do
+        content <- L.readFile name
+        forkIO (compressFile name content)
+        return ()
+
+      main
+  where
+    compressFile path = L.writeFile (path ++ ".gz") . compress
