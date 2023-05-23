b01aee1ce4db041273a2f1c2ad1e176d0d6cac68
beekill <nguyenmbquan95@gmail.com>
2023-05-23 09:11:55 -0700

    Implement NiceFork

--- fa14_w12_concurrency_parallelism/src/NiceFork.hs
+++ fa14_w12_concurrency_parallelism/src/NiceFork.hs
@@ -10,6 +10,7 @@ where
 
 import Control.Concurrent
 import Control.Exception (SomeException, try)
+import Control.Monad
 import qualified Data.Map as M
 
 newtype ThreadManager
@@ -28,16 +29,47 @@ newManager = Mgr <$> newMVar M.empty
 
 -- | Create a new managed thread.
 forkManaged :: ThreadManager -> IO () -> IO ThreadId
-forkManaged = undefined
+forkManaged (Mgr mgr) body =
+  modifyMVar mgr $ \m -> do
+    state <- newEmptyMVar
+    tid <- forkIO $ do
+      result <- try body
+      putMVar state $ either Threw (const Finished) result
+    return (M.insert tid state m, tid)
 
 -- | Immediately return the status of a managed thread.
 getStatus :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
-getStatus = undefined
+getStatus (Mgr mgr) tid =
+  modifyMVar mgr $ \m -> case M.lookup tid m of
+    Nothing -> return (m, Nothing)
+    Just st ->
+      tryTakeMVar st
+        >>= \mst -> return $ case mst of
+          Nothing -> (m, Just Running)
+          x@(Just _) -> (M.delete tid m, x)
 
 -- | Block until a specific managed thread terminates.
 waitFor :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
-waitFor = undefined
+waitFor (Mgr mgr) tid =
+  -- Old waitFor:
+  -- do
+  -- maybeDone <- modifyMVar mgr $ \m ->
+  --   return $ case M.updateLookupWithKey (\_ _ -> Nothing) tid m of
+  --     (Nothing, _) -> (m, Nothing)
+  --     (done, m') -> (m', done)
+
+  -- case maybeDone of
+  --   Nothing -> return Nothing
+  --   Just st -> Just <$> takeMVar st
+
+  -- New waitFor(2):
+  join . modifyMVar mgr $ \m ->
+    return $ case M.updateLookupWithKey (\_ _ -> Nothing) tid m of
+      (Nothing, _) -> (m, return Nothing)
+      (Just st, m') -> (m', Just <$> takeMVar st)
 
 -- | Block until all managed threads terminate.
 waitAll :: ThreadManager -> IO ()
-waitAll = undefined
+waitAll (Mgr mgr) = modifyMVar mgr elems >>= mapM_ takeMVar
+  where
+    elems m = return (M.empty, M.elems m)
