470413db4200a9ccef7dbf9ce623116637768521
beekill <nguyenmbquan95@gmail.com>
2023-05-24 20:13:03 -0700

    Use Gtk instead of wxWidgets

--- fa16_hw11_functional_reactive_programming/app/Main.hs
+++ fa16_hw11_functional_reactive_programming/app/Main.hs
@@ -1,8 +1,27 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import qualified MyLib (someFunc)
+import Data.GI.Base
+import qualified GI.Gtk as Gtk
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  Gtk.init Nothing
+  win <- new Gtk.Window [#title := "Hi, there"]
+  on win #destroy Gtk.mainQuit
+  button <- new Gtk.Button [#label := "Click me"]
+  on
+    button
+    #clicked
+    ( set
+        button
+        [ #sensitive := False,
+          #label := "Thanks for clicking me"
+        ]
+    )
+
+  #add win button
+  #showAll win
+  Gtk.main
