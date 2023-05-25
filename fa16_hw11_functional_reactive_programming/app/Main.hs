bc3479a348478b6bfc47be54eb53a4d63ec6e72e
beekill <nguyenmbquan95@gmail.com>
2023-05-25 07:44:42 -0700

    Convert UI in the lecture note from wxWidgets to Gtk

--- fa16_hw11_functional_reactive_programming/app/Main.hs
+++ fa16_hw11_functional_reactive_programming/app/Main.hs
@@ -3,25 +3,78 @@
 
 module Main where
 
+import Control.Monad
 import Data.GI.Base
+import Data.Text
 import qualified GI.Gtk as Gtk
 
 main :: IO ()
-main = do
-  Gtk.init Nothing
-  win <- new Gtk.Window [#title := "Hi, there"]
+main = coinUI
+
+-- main = do
+--   Gtk.init Nothing
+--   win <- new Gtk.Window [#title := "Hi, there"]
+--   on win #destroy Gtk.mainQuit
+--   button <- new Gtk.Button [#label := "Click me"]
+--   on
+--     button
+--     #clicked
+--     ( set
+--         button
+--         [ #sensitive := False,
+--           #label := "Thanks for clicking me"
+--         ]
+--     )
+
+--   #add win button
+--   #showAll win
+--   Gtk.main
+
+coinUI :: IO ()
+coinUI = do
+  _ <- Gtk.init Nothing
+
+  win <- new Gtk.Window [#title := "Banana"]
   on win #destroy Gtk.mainQuit
-  button <- new Gtk.Button [#label := "Click me"]
+
+  l <- new Gtk.ListBox []
+  money <- new Gtk.Entry [#text := "5"]
+
+  add <- new Gtk.Button [#label := "Insert Coin"]
+  buy <- new Gtk.Button [#label := "Buy Banana", #sensitive := False]
+  quit <- new Gtk.Button [#label := "Quit"]
+  on quit #clicked Gtk.mainQuit
+
   on
-    button
+    add
     #clicked
-    ( set
-        button
-        [ #sensitive := False,
-          #label := "Thanks for clicking me"
-        ]
+    ( do
+        v <- read . unpack <$> get money #text
+        let v' = v + 1 :: Integer
+        set money [#text := (pack . show) v']
+        when (v' >= 10) $ set buy [#sensitive := True]
     )
 
-  #add win button
+  on
+    buy
+    #clicked
+    ( do
+        v <- read . unpack <$> get money #text
+        let v' = v - 10 :: Integer
+        set money [#text := (pack . show) v']
+        new
+          Gtk.MessageDialog
+          [ #title := "Yummy",
+            #text := "You bought a banana!"
+          ]
+        return ()
+    )
+
+  #add l add
+  #add l buy
+  #add l money
+  #add l quit
+  #add win l
+
   #showAll win
   Gtk.main
